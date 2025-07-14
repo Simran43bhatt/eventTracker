2b7ac7425c63d450c2c9b737fe8ad31be987a569
Simran43bhatt <simran.43bhatt@gmail.com>
2025-07-14 15:07:10 +0530

    oush

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      ...typescriptEslint.configs.recommended.rules,
+      '@typescript-eslint/explicit-member-accessibility': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-parameter-properties': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/*.js',
+      '**/*.d.ts',
+      "**/eslint.config.js"
+    ],
+  },
+]; 
